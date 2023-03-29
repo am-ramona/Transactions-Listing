40674f6e1baf156613b3b79702c0698ef9c68244
am-ramona <ramona.abimoussa@gmail.com>
2023-03-29 14:05:31 +0300

    initial commit

---
+++ pages/transactions/[address]/[transaction].tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { Network } from 'alchemy-sdk'
+import { AlchemyMultichainClient } from '../../../alchemy-multichain-client'
+import styles from '@/styles/Home.module.css'
+
+const defaultConfig = {
+    apiKey: "oC6F3KjezCjGO5b-S0Wn4uunajlNUB6A",
+    network: Network.ETH_MAINNET,
+};
+const overrides = {
+    [Network.MATIC_MAINNET]: { apiKey: 'vXOn8o6kX_znYRMaB3lf2i7srDCZsxVH' },
+  };
+
+  const alchemy = new AlchemyMultichainClient(defaultConfig, overrides);
+
+export default function CommentPage() {
+    const router = useRouter()
+    const transaction = router.query.transaction as string
+    const amount = router.query.amount as string
+    const timestamp = router.query.timestamp as string
+    const block_explorer = router.query.block_explorer as string
+
+    const [ receipt, setReceipt ] = useState<any>()
+
+    useEffect(() => {
+        getTransactionReceipt(transaction).catch(console.error);
+    }, [transaction]);
+
+
+    async function getTransactionReceipt(hash: any) {
+        if (typeof hash === 'undefined') return;
+
+        const network = block_explorer === 'Etherscan' ? Network.ETH_MAINNET:Network.MATIC_MAINNET
+        const response = await alchemy.forNetwork(network).core
+            .getTransactionReceipt(
+                hash // Transaction hash of the transaction for which you want to get information.
+            )
+          .then((response) => {
+            setReceipt(response);
+        })
+    }
+
+    return (
+        <section className={styles.padding}>
+            <code className={styles.center}>Transaction details</code>
+            <span>
+                <a href={block_explorer === 'Etherscan' ? 'https://etherscan.io/': 'https://polygonscan.com/'} target="_blank" className={`${styles.underline} ${styles.wrap}`}>{transaction}</a>
+            </span>
+            <p> Amount: {amount}</p>
+            <p> Timestamp: {timestamp}</p>
+            <p> Status: { receipt && receipt.status === 1 ? 'Success': 'Failure'} </p>
+            <p> Transaction fee: { receipt && receipt.effectiveGasPrice._hex }</p>
+        </section>
+    )
+}
---
+++ pages/transactions/[address]/index.tsx
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import { Alchemy, Network, AssetTransfersCategory } from 'alchemy-sdk'
+import { AlchemyMultichainClient } from '../../../alchemy-multichain-client.ts'
+import styles from '@/styles/Home.module.css'
+
+const defaultConfig = {
+    apiKey: "oC6F3KjezCjGO5b-S0Wn4uunajlNUB6A",
+    network: Network.ETH_MAINNET,
+};
+const overrides = {
+    [Network.MATIC_MAINNET]: { apiKey: 'vXOn8o6kX_znYRMaB3lf2i7srDCZsxVH' },
+  };
+
+  const alchemy = new AlchemyMultichainClient(defaultConfig, overrides);
+
+//The below token contract address corresponds to USDT
+const tokenContractAddresses = ["0xdAC17F958D2ee523a2206206994597C13D831ec7"];
+
+export default function PostPage() {
+    const router = useRouter()
+    const address = router.query.address as string
+    const block_explorer = router.query.block_explorer as string
+
+    const [data, setData] = useState<any>({})
+    const [balance, setBalance] = useState<any>({})
+    const [order, setOrder] = useState<boolean>(true)
+
+    useEffect(() => {
+        getEthereumData(true).catch(console.error);
+    }, [address]);
+
+    async function getEthereumData(order: boolean) {
+        if (typeof address === 'undefined') return;
+
+        const network = block_explorer === 'Etherscan' ? Network.ETH_MAINNET:Network.MATIC_MAINNET
+
+        try {
+            const data = await alchemy.forNetwork(network).core.getAssetTransfers({
+                fromBlock: "0x0",
+                fromAddress: address,
+                category: [AssetTransfersCategory.EXTERNAL, AssetTransfersCategory.INTERNAL, AssetTransfersCategory.ERC20, AssetTransfersCategory.ERC721, AssetTransfersCategory.ERC1155],
+                order: order === true ? 'asc' : 'desc',
+                withMetadata: true,
+                maxCount: 100
+            }).then((data) => {
+                setData(data);
+                setOrder(order)
+            })
+
+            const balance = await alchemy.forNetwork(network).core.getTokenBalances(
+                address,
+                tokenContractAddresses
+            ).then((balance:any) => {
+                setBalance(balance);
+            })
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <section className={styles.frame}>
+            <code className={styles.center}>From: {address}</code>
+            <code className={styles.italic}>Transactions operated on Ethereum, 
+               <span className={styles.green}>limited to 100:</span>
+            </code>
+
+            <main className={styles.card}>
+                <div className={styles.grid}>
+                    <span> Amount </span>
+                    <span> Timestamp </span>
+                    <span></span>
+                </div>
+
+                <div>
+                    {(address
+                        && data.transfers
+                        && data.transfers.length !== 0)
+                        && data.transfers.map((item, index) =>
+                            <div key={index} className={styles.grid}>
+                                <span>{item.value}</span>
+                                <span>{item.metadata.blockTimestamp}</span>
+                                <Link href={{ pathname: `${address}/${item.hash}`, query: { amount: item.value, timestamp: item.metadata.blockTimestamp, block_explorer: block_explorer } }}> see more </Link>
+                            </div>
+                        )}
+                </div>
+
+            </main>
+
+            <main>
+                <p> Address current balance is: {balance.tokenBalances && balance.tokenBalances[0].tokenBalance}</p>
+                <p> Sort by <span className={`${styles.red} ${styles.pointer}`} onClick={() => getEthereumData(!order)}>Timestamp</span></p>
+            </main>
+        </section>
+    )
+}
